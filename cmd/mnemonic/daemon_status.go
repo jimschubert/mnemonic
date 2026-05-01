b32e14fddf6ee3c528f9c39f7d52b6374ce31c1e
Jim Schubert <james.schubert@gmail.com>
2026-04-30 20:07:22 -0400

    refactor: use cleaner fmt.Fprintf in status cmd

--- cmd/mnemonic/daemon_status.go
+++ cmd/mnemonic/daemon_status.go
@@ -18,27 +18,27 @@ type DaemonStatusCmd struct{}
 
 //goland:noinspection GoUnhandledErrorResult
 func (c *DaemonStatusCmd) Run(_ *slog.Logger, conf config.Config) error {
-	writer := tabwriter.NewWriter(os.Stdout, 0, 0, 4, ' ', 0)
-	defer writer.Flush() //nolint:errcheck
+	w := tabwriter.NewWriter(os.Stdout, 0, 0, 4, ' ', 0)
+	defer w.Flush() //nolint:errcheck
 
 	// may differ from config used to start the daemon, but likely never changes in the config file so okay for now
-	writer.Write(fmt.Appendf(nil, "socket:\t%s\n", conf.SocketPath())) //nolint:errcheck
+	fmt.Fprintf(w, "socket:\t%s\n", conf.SocketPath()) //nolint:errcheck
 
 	if !daemon.IsRunning(conf) {
-		writer.Write(fmt.Appendf(nil, "status:\tnot running\n")) //nolint:errcheck
+		fmt.Fprintf(w, "status:\tnot running\n") //nolint:errcheck
 		return nil
 	}
 
 	client := daemon.NewSocketClient(conf)
 	resp, err := client.Get("http://unix/api/status")
 	if err != nil {
-		writer.Write(fmt.Appendf(nil, "status:\tunreachable (%s)\n", err)) //nolint:errcheck
+		fmt.Fprintf(w, "status:\tunreachable (%s)\n", err) //nolint:errcheck
 		return nil
 	}
 	defer resp.Body.Close() //nolint:errcheck
 
 	if resp.StatusCode != http.StatusOK {
-		writer.Write(fmt.Appendf(nil, "status:\terror connecting... response code was %d\n", resp.StatusCode)) //nolint:errcheck
+		fmt.Fprintf(w, "status:\terror connecting... response code was %d\n", resp.StatusCode) //nolint:errcheck
 		return nil
 	}
 
@@ -54,10 +54,10 @@ func (c *DaemonStatusCmd) Run(_ *slog.Logger, conf config.Config) error {
 	}
 
 	startedAt := time.Unix(statusResp.StartedAt, 0).Format(time.RFC3339)
-	writer.Write(fmt.Appendf(nil, "status:\t%s\n", statusResp.Status))   //nolint:errcheck
-	writer.Write(fmt.Appendf(nil, "version:\t%s\n", statusResp.Version)) //nolint:errcheck
-	writer.Write(fmt.Appendf(nil, "uptime:\t%s\n", statusResp.Uptime))   //nolint:errcheck
-	writer.Write(fmt.Appendf(nil, "started:\t%s\n", startedAt))          //nolint:errcheck
+	fmt.Fprintf(w, "status:\t%s\n", statusResp.Status)   //nolint:errcheck
+	fmt.Fprintf(w, "version:\t%s\n", statusResp.Version) //nolint:errcheck
+	fmt.Fprintf(w, "uptime:\t%s\n", statusResp.Uptime)   //nolint:errcheck
+	fmt.Fprintf(w, "started:\t%s\n", startedAt)          //nolint:errcheck
 
 	return nil
 }
